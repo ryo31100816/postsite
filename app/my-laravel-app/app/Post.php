077d0d6842779c2a1d17c87637123897455eb9b4
Ryo <ryo3110yamanashi@gmail.com>
2020-10-25 19:34:55 +0900

    home view

--- app/my-laravel-app/app/Post.php
+++ app/my-laravel-app/app/Post.php
@@ -8,4 +8,9 @@ class Post extends Model
 {
     //テーブル名をプロパティ記述しなくてもよい
     //指定すればそのテーブルを見に行くようにもできる
+    public function images()
+    {
+        return $this->hasMany('App\Image', 'post_id', 'id');
+    }
+
 }
--- app/my-laravel-app/resources/views/home.blade.php
+++ app/my-laravel-app/resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                {{ $title }}
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -16,6 +16,11 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+                @if (isset($posts))
+                    @foreach($posts as $post)
+                        <img src="{{ asset($post->images[0]->icon) }}" class="img-thumbnail"></img>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
